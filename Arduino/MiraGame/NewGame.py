61e0f32f7952f5c8a07e6725594bf6b276b2a4f4
qhdo1010 <do.q@husky.neu.edu>
2019-04-02 23:18:38 -0400

    Press RightKey to Run the Animation

---
+++ Arduino/MiraGame/NewGame.py
@@ -0,0 +1,53 @@
+import pyglet
+from pyglet import clock
+i = 0
+fps_display = clock.ClockDisplay()
+class ImageSprite(pyglet.sprite.Sprite):
+    def __init__(self, batch, img):
+        pyglet.sprite.Sprite.__init__(self, img, x = 300, y = 300)
+
+    def black(self):
+        self.image = pyglet.image.load("black.png")
+        return self
+
+    def white(self):
+        self.image = pyglet.image.load('white.png')    
+        return self
+
+class Game(pyglet.window.Window):
+    def __init__(self):
+        pyglet.window.Window.__init__(self, width = 600, height = 600)
+	fps_display = clock.ClockDisplay()
+        self.batch_draw = pyglet.graphics.Batch()
+        self.player = ImageSprite(batch = self.batch_draw, img = pyglet.image.load("white.png"))
+        self.keys_held = []      
+        self.schedule = pyglet.clock.schedule_interval(func = self.update, interval = 1/60.) 
+
+    def on_draw(self): 
+        self.clear()      
+	fps_display.draw()   
+        self.batch_draw.draw()
+        self.player.draw()  
+
+    def on_key_press(self, symbol, modifiers):
+        self.keys_held.append(symbol)
+       # if symbol == pyglet.window.key.RIGHT:
+       #     self.player = self.player.move()
+#            print "The 'RIGHT' key was pressed"
+
+    def on_key_release(self, symbol, modifiers):
+        self.keys_held.pop(self.keys_held.index(symbol))
+       # self.player = self.player.stand()
+ #       print "The 'RIGHT' key was released"
+
+    def update(self, interval):
+        if pyglet.window.key.RIGHT in self.keys_held:
+		global i
+		if i%2 == 0:
+			self.player = self.player.black()
+		else:
+			self.player = self.player.white()
+		i=i+1
+if __name__ == "__main__":
+    window = Game()
+    pyglet.app.run()

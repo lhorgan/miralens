929de96490f82909e01edb8f0e33ad4eb3213e46
josh-english <joshua.c.english@gmail.com>
2019-04-07 12:05:47 -0400

    gitignore and spacing changes

--- Arduino/MiraGame/NewGame.py
+++ Arduino/MiraGame/NewGame.py
@@ -1,53 +1,61 @@
 import pyglet
 from pyglet import clock
 i = 0
+fps = 240
+clock.set_fps_limit(fps)
 fps_display = clock.ClockDisplay()
+
 
 class ImageSprite(pyglet.sprite.Sprite):
     def __init__(self, batch, img):
-        pyglet.sprite.Sprite.__init__(self, img, x = 300, y = 300)
+        pyglet.sprite.Sprite.__init__(self, img, x=300, y=300)
         self.b = pyglet.image.load("black.png")
         self.w = pyglet.image.load("white.png")
-	
+
     def black(self):
         self.image = self.b
         return self
 
     def white(self):
-        self.image = self.w    
+        self.image = self.w
         return self
+
 
 class Game(pyglet.window.Window):
     def __init__(self):
-        pyglet.window.Window.__init__(self, width = 600, height = 600)
-	fps_display = clock.ClockDisplay()
+        pyglet.window.Window.__init__(self, width=600, height=600)
+        # fps_display = clock.ClockDisplay()
         self.batch_draw = pyglet.graphics.Batch()
-        self.player = ImageSprite(batch = self.batch_draw, img = pyglet.image.load("white.png"))
-        self.keys_held = []      
-        self.schedule = pyglet.clock.schedule_interval(func = self.update, interval = 1/60.) 
+        self.player = ImageSprite(batch=self.batch_draw, img=pyglet.image.load("white.png"))
+        self.keys_held = []
+        self.schedule = pyglet.clock.schedule_interval(func=self.update, interval=1/float(fps*2))
 
-    def on_draw(self): 
-        self.clear()      
-	fps_display.draw()   
+    def on_draw(self):
+        self.clear()
+        fps_display.draw()
         self.batch_draw.draw()
-        self.player.draw()  
+        self.player.draw()
 
     def on_key_press(self, symbol, modifiers):
         self.keys_held.append(symbol)
-#            print "The 'RIGHT' key was pressed"
+        # print "The 'RIGHT' key was pressed"
 
     def on_key_release(self, symbol, modifiers):
         self.keys_held.pop(self.keys_held.index(symbol))
- #       print "The 'RIGHT' key was released"
+        # print "The 'RIGHT' key was released"
 
     def update(self, interval):
         if pyglet.window.key.RIGHT in self.keys_held:
-		global i
-		if i%2 == 0:
-			self.player = self.player.black()
-		else:
-			self.player = self.player.white()
-		i=i+1
+            global i
+            if i % 2 == 0:
+                self.player = self.player.black()
+                i = 0
+            else:
+                self.player = self.player.white()
+                i = 1
+            i += 1
+
+
 if __name__ == "__main__":
     window = Game()
     pyglet.app.run()

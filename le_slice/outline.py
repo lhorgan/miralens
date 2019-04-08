7930e0d1117fc413f410c89bd43176bc57df0534
Luke Horgan <horgan.l@husky.neu.edu>
2019-04-07 21:33:48 -0400

    grkhrk make blue

--- le_slice/outline.py
+++ le_slice/outline.py
@@ -19,9 +19,9 @@ def create_outline(input_dir, in_name, out_name):
         for j in range(3, img.size[1] - 3):    # For every row
             #print(pixels[i,j])
             if not is_white(pixels[i,j]) and (is_white(pixels[i-1,j]) or is_white(pixels[i+1,j]) or is_white(pixels[i,j-1]) or is_white(pixels[i,j+1])):
-                for l in range(i-3, i+3):
+                for l in range(i-5, i+5):
                     for m in range(j-3, j+3):
-                        new_img_pixels[l,m] = pixels[l,m]
+                        new_img_pixels[l,m] = (0, 0, 255)#pixels[l,m]
     
     new_img.save(out_name)
 

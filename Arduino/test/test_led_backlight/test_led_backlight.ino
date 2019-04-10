33be185eaabb29b361c7ff999677c188f162e17e
matinraayai <raayai.matin@gmail.com>
2019-04-10 16:21:56 -0400

    Changed test code to prove the whole grid can be lit as well as the resetting functionality.

--- Arduino/test/test_led_backlight/test_led_backlight.ino
+++ Arduino/test/test_led_backlight/test_led_backlight.ino
@@ -1,14 +1,14 @@
 #include "BackLight.hpp"
 int pins[20] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 38, 39, 40, 41, 42, 43, 44, 45};
 BackLight bl = BackLight(pins);
-const unsigned long interval = 1;
+const unsigned long interval = 100;
 
 void setup() { 
   bl.setup();
 }
 
 void loop() {
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < 22; i++) {
         bl.nextLed();
         delay(interval);
     }
